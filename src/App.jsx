35d45df7e4c9763c9adb1aa338144c7ddb0f3d23
Aiden Nguyen <aiden.nguyen.dev@gmail.com>
2025-08-13 12:54:43 +0700

    set background, logo & banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold text-gray-900">Welcome to iMovie</h1>
-        <p className="mt-4 text-lg text-gray-600">Get started by editing <code>src/App.jsx</code></p>
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="./src/assets/avengers.png" alt="Avengers Banner" className="w-70 h-70 "/>
+            <h1 className="text-4xl font-bold text-white-900">
+              Welcome to iMovie
+            </h1>
+            <h3>
+              Find <span className="text-gradient">Movies</span> You Will Enjoy!
+            </h3>
+          </header>
+          <p>Search</p>
+        </div>
       </div>
-  </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
