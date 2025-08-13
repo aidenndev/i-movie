12e6a0a5e8c6bba5b21b8c5f2fe1ec300482bac4
Aiden Nguyen <aiden.nguyen.dev@gmail.com>
2025-08-13 16:08:59 +0700

    add Search props and TMDB API

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import { useState } from "react";
 import "./App.css";
+import Search from "./components/Search";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <main>
       <div className="pattern">
         <div className="wrapper">
           <header>
-            <img src="./src/assets/avengers.png" alt="Avengers Banner" className="w-70 h-70 "/>
-            <h1 className="text-4xl font-bold text-white-900">
+            <img
+              src="./src/assets/avengers.png"
+              alt="Avengers Banner"
+              className="w-70 h-70"
+            />
+            <h1 className="text-4xl font-bold text-white-900 justify-center">
               Welcome to iMovie
             </h1>
-            <h3>
-              Find <span className="text-gradient">Movies</span> You Will Enjoy!
+            <h3 className="text-2xl text-white-700">
+              Find <span className="text-gradient">Movies</span> You Will
+              Enjoy!
             </h3>
           </header>
-          <p>Search</p>
         </div>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   );
---
+++ src/components/Search.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search-container">
+      <img
+        src="./src/assets/search.svg"
+        alt="Search Icon"
+        className="w-10 h-10"
+      />
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="search-input"
+        placeholder="Search for your favorite movies..."
+      />
+    </div>
+  );
+};
+
+export default Search;
