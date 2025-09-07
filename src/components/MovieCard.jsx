6ea813b9d6f1910ed0d115df70a96668c3828dcb
Aiden Nguyen <aiden.nguyen.dev@gmail.com>
2025-09-07 18:20:19 +0930

    Add MovieCard infor (rating, year, language)

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from "react";
 
-const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, original_language } }) => {
+const MovieCard = ({
+  movie: { title, vote_average, poster_path, release_date, original_language },
+}) => {
   return (
-    <div className='movie-card'>
-        <h4>{title}</h4>
-        <p>Rating: {vote_average}</p>
-        <p>Release Date: {release_date}</p>
-        <p>Language: {original_language}</p>
-        <img
-          src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-          alt={title}
-        />
+    <div className="movie-card">
+      <img
+        src={
+          poster_path
+            ? `https://image.tmdb.org/t/p/w500${poster_path}`
+            : "./src/assets/not-found.png"
+        }
+        alt={title}
+      />
+      <h3>{title}</h3>
+      <div className="movie-info">
+        <div className="rating">
+          <img src="./src/assets/star.png" alt="Star Icon" />
+          <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+        </div>
+        <span>•</span>
+        <p className="language">{original_language}</p>
+        <span>•</span>
+        <p className="year">
+          {release_date ? release_date.split("-")[0] : "N/A"}
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
