5519bb33ddc75b00faea5d5b014073e15febee64
Aiden Nguyen <aiden.nguyen.dev@gmail.com>
2025-09-08 23:53:29 +0930

    Fix outdate API call syntax

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,42 +1,44 @@
-import { Client, Databases, ID, Query } from "appwrite";
+import { Client, TablesDB, ID, Query } from "appwrite";
 
 const ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_DATABASE_ID;
-const COLLECTION_ID = import.meta.env.VITE_COLLECTION_ID;
+const TABLE_ID = import.meta.env.VITE_TABLE_ID;
 
 //Define new Appwrite Client
 const client = new Client()
     .setEndpoint(ENDPOINT)
     .setProject(PROJECT_ID);
 
-const database = new Databases(client);
+const database = new TablesDB(client);
 
 export const updateSearchCount = async (searchTerm, movie) => {
     try {
 
-        //1. Get the current search count if it exists then update
-        const response = await database.getDocument(
-            DATABASE_ID,
-            COLLECTION_ID,
-            {queries: [Query.equal("searchTerm", searchTerm)]}
-        );
-        const documents = response.documents;
+        //Get the current search count if it exists then update
+        const result = await database.listRows({
+            databaseId: DATABASE_ID,
+            tableId: TABLE_ID,
+            queries: [Query.equal("searchTerm", searchTerm)]
+        });
 
-        if (documents.length > 0) {
-            // Document exists, we can update it
-            const doc = documents[0];
-            await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
-                searchCount: doc.searchCount + 1
+        console.log("Appwrite result:", result);
+        const searchResults = result.rows;
+
+        if (searchResults.length > 0) {
+            //searchTerm exists, update the count
+            const doc = searchResults[0];
+            await database.updateRow(DATABASE_ID, TABLE_ID, doc.$id, {
+                count: doc.count + 1
             });
-            console.log(`Search count updated to ${doc.searchCount + 1}`);
+            console.log(`Search count updated to ${doc.count + 1}`);
 
         }
-        //2. If no document exists, create a new one
+        //If not, create a new one
         else {
-            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+            await database.createRow(DATABASE_ID, TABLE_ID, ID.unique(), {
                 searchTerm,
-                searchCount: 1,
+                count: 1,
                 movie_id: movie.id,
                 poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
             });
